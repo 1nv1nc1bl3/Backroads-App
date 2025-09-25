adbc6e6af6d6d64c886fdc55d34b29cc5ef4128d
Alkis <alkis.k1972@gmail.com>
2025-09-25 19:35:29 +0300

    First commit of Backroads Project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Tours from './components/Tours';
+import Footer from './components/Footer';
+import About from './components/About';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Services from './components/Services';
+
+export default function App() {
+    return (
+        <>
+            <body>
+                <Navbar />
+                <Hero />
+                <About />
+                <Services />
+                <Tours />
+                <Footer />
+            </body>
+        </>
+    );
+}
